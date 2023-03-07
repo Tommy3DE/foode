f0d30d18dfb9ec65e5f20d51a946d75fc892eb4e
Tommy3DE <84966413+Tommy3DE@users.noreply.github.com>
2023-03-07 15:56:07 +0100

    dodanie stron pojedynczych potraw i zmiany w nawigacji

--- app/types/[type]/[id]/page.jsx
+++ app/types/[type]/[id]/page.jsx
@@ -1,19 +1,54 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const fetchRecipeDetails = async (id) => {
-    const res = await fetch(
-      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
-    );
-    const response = await res.json();
-    return response
-  };
+  const res = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+  );
+  const response = await res.json();
+  return response;
+};
 
-const page = async({params}) => {
-    const details = await fetchRecipeDetails(params.id) //szczegóły potraw
-    // console.log(details)
+const page = async ({ params }) => {
+  const recipeDetails = await fetchRecipeDetails(params.id); //szczegóły potraw
+  const details = recipeDetails.meals[0];
+//   console.log(details); szczegóły dań
+
+  const ingredients = Object.keys(details).filter(
+    (key)=> key.indexOf('Ingredient') > 0
+  ).map((ingKey)=>(
+    details[ingKey]
+  )).filter(Boolean)
+//   console.log(ingredients); wartosci skladników w których cos było
   return (
-    <div>page</div>
-  )
-}
+    <div className="grid grid-cols-1 md:grid-cols-2">
+      <div>
+        <Image
+          src={details.strMealThumb}
+          width={500}
+          height={500}
+          alt={details.strMeal}
+          className='rounded-r-lg'
+        />
+      </div>
+      <div className="p-5">
+        <h1 className="font-bold text-xl">Recipe Name: <span className="text-3xl"> {details.strMeal}</span></h1>
+        <div className="mt-3 ">
+            <p className="text-xl my-3">Ingredients:</p>
+            {ingredients.map((ingredient, index)=>(
+                <span className="bg-slate-400 text-white hover:bg-blue-400 px-2 py-1 rounded inline-block mr-2 mb-2" key={index}>
+                    {ingredient}
+                </span>
+            ))}
+        </div>
+        <p className="mt-3">{details.strInstructions}</p>
+        <div className="mt-3 text-slate-500">
+            <p>Video Link:</p>
+            <a target='_blank' href={details.strYoutube}>{details.strMeal}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
--- components/Header.jsx
+++ components/Header.jsx
@@ -5,13 +5,19 @@ import React from 'react'
 import { usePathname } from 'next/navigation'
 
 const Header = () => {
-  const pathname = usePathname()
+  const pathname = usePathname().split('/')
+  const currentArea = pathname[2]
+  const recipeId = pathname[3]
   console.log(pathname)
   return (
-    <div className='py-5 bg-slate-300'>
+    <div className='py-5 bg-slate-300 flex items-center justify-between px-2'>
         <Link href='/'>
             <h1 className='text-blue-700 font-bold text-5xl'>Foode</h1>
         </Link>
+      {pathname && currentArea && 
+        <Link className='bg-blue-500 text-white p-4 text-xs rounded font-bold' href={recipeId ? `/types/${currentArea}` : `/types`}>
+            Back to {recipeId ? `${currentArea} recipes`: 'countries browser'}
+        </Link>}
     </div>
   )
 }
