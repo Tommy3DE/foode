35fb5ce21871c187860535fa4d5c0a21abee2cb3
Tommy3DE <84966413+Tommy3DE@users.noreply.github.com>
2023-03-09 11:04:55 +0100

    zmiany stylowania

--- app/types/[type]/[id]/page.jsx
+++ app/types/[type]/[id]/page.jsx
@@ -22,31 +22,32 @@ const page = async ({ params }) => {
   )).filter(Boolean)
 //   console.log(ingredients); wartosci skladników w których cos było
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2">
+    <div className="flex flex-col mt-5 items-center justify-center">
       <div>
         <Image
           src={details.strMealThumb}
           width={500}
           height={500}
           alt={details.strMeal}
-          className='rounded-r-lg'
+          className='rounded-lg'
         />
       </div>
-      <div className="p-5">
-        <h1 className="font-bold text-xl">Recipe Name: <span className="text-3xl"> {details.strMeal}</span></h1>
-        <div className="mt-3 ">
-            <p className="text-xl my-3">Ingredients:</p>
+      <div className="flex flex-col justify-center items-center w-4/5">
+        <h1 className="text-3xl my-3 font-bold">Recipe Name: <span className="text-3xl"> {details.strMeal}</span></h1>
+        <div className="mt-3 w-2/3 items-center">
+            <p className="text-3xl text-center my-4 ">Ingredients:</p>
             {ingredients.map((ingredient, index)=>(
                 <span className="bg-slate-400 text-white hover:bg-blue-400 px-2 py-1 rounded inline-block mr-2 mb-2" key={index}>
                     {ingredient}
                 </span>
             ))}
         </div>
-        <p className="mt-3">{details.strInstructions}</p>
+        <p className="mt-3 w-4/5 text-lg">{details.strInstructions}</p>
         {details.strYoutube && 
-            <div className="mt-3 text-slate-500">
-                <p>Video Link:</p>
-                <a target='_blank' href={details.strYoutube}>{details.strMeal}</a>
+            <div className="mt-5 text-lg">
+                <p>Video Link: {' '}
+                <a target='_blank' className="text-blue-600 font-bold" href={details.strYoutube}>{details.strMeal}</a>
+                </p>
             </div>
         }
       </div>
--- app/types/page.jsx
+++ app/types/page.jsx
@@ -7,15 +7,16 @@ const fetchRecipeAreas = async () => {
   );
   const response = await res.json();
   return response.meals.map((a) => a.strArea);
-  
 };
 
 const page = async () => {
   const areas = await fetchRecipeAreas();
-  console.log(areas.length)
+  console.log(areas.length);
   return (
     <div className="bg-[url('../public/bg3.jpeg')] bg-cover h-screen">
-      <h1 className="text-5xl mt-3 text-center py-3 font-bold text-white ">Pick country of origin</h1>
+      <h1 className="text-5xl mt-3 text-center py-3 font-bold text-white ">
+        Pick country of origin
+      </h1>
       <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 p-5">
         {areas.map((a, index) => (
           <Link
